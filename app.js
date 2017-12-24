7a08ef9f280baa382bdbdf02655e1a8ab98661d2
aaronpio <aaronpiolang@gmail.com>
2017-12-23 22:45:46 -0500

    More updates yo

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ var budgetController = (function() {
 
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            };
+        },
+
         calculateBudget: function () {
 
             //calculate total income and expenses
@@ -111,7 +125,8 @@ var UIController = (function() {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
 
     };
 
@@ -130,10 +145,10 @@ var UIController = (function() {
             // create html string with placeholder text
         if (type === 'inc'){
             element = DOMstrings.incomeContainer;
-            html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         } else if (type === 'exp') {
             element = DOMstrings.expensesContainer;
-            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
         }
             //replace placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
@@ -143,6 +158,13 @@ var UIController = (function() {
             //insert html into DOM
         document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
             
+        },
+
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            
+            el.parentNode.removeChild(el);
+        
         },
 
         clearFields: function() {
@@ -198,7 +220,9 @@ var controller = (function(budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
                     
-        })
+        });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
         
     };
 
@@ -230,6 +254,28 @@ var controller = (function(budgetCtrl, UICtrl) {
             updateBudget();
         }
 
+    };
+
+    var ctrlDeleteItem = function (event) {
+        var itemID, splitID, type, ID;
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (itemID) {
+            //inc-1
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = parseInt(splitID[1]);
+
+            //delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
+
+            //delete item from UI
+            UICtrl.deleteListItem(itemID);
+
+            //update and show new budget
+            updateBudget();
+
+        }
     };
 
     return {
