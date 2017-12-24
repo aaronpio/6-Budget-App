dd980b98861de3ab5e715b4996134936f37e848c
aaronpio <aaronpiolang@gmail.com>
2017-12-23 19:03:36 -0500

    Update

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ var budgetController = (function() {
 
             // calculate the percentage of income that we spent
             if ( data.totals.inc > 0) {
-            data.percentage = data.totals.exp / data.totals.inc;
+            data.percentage = data.totals.exp / data.totals.inc * 100;
             } else {
                 data.percentage = -1;
             } 
@@ -107,7 +107,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 
     };
 
@@ -154,6 +158,20 @@ var UIController = (function() {
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -190,7 +208,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // return budget
             var budget = budgetCtrl.getBudget();
         // display budget
-            console.log(budget);
+            UICtrl.displayBudget(budget);
         };
 
     var ctrlAddItem = function() {
@@ -208,7 +226,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             // Clear the fields
             UICtrl.clearFields();
 
-            // calvulate and update budget
+            // calculate and update budget
             updateBudget();
         }
 
@@ -217,6 +235,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('its started motherfucker');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
+
             setupEventListeners();
         }
     }
