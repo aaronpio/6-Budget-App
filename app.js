221cfe04430fbf09e922acf01eb5abb329928a45
aaronpio <aaronpiolang@gmail.com>
2017-12-25 16:55:01 -0500

    Xmas day

--- app.js
+++ app.js
@@ -155,7 +155,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
 
     };
 
@@ -223,6 +224,25 @@ var UIController = (function() {
             }
         },
 
+        displayPercentages: function(percentages) {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i , i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current,index) {
+
+                if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -274,9 +294,9 @@ var controller = (function(budgetCtrl, UICtrl) {
             var percentages = budgetCtrl.getPercentages();
 
             //update the UI with new percentages
-            console.log(percentages);
+            UICtrl.displayPercentages(percentages);
 
-        }
+        };
 
     var ctrlAddItem = function() {
         var input, newItem;
